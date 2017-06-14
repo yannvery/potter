4f063a6d7a1f64921358650be481338bc7e5f950
meo84 <oopsiday@gmail.com>
2017-06-14 20:31:56 +0200

    setup ruby version and apply first discount

--- lib/price_calculator.rb
+++ lib/price_calculator.rb
@@ -1,5 +1,13 @@
 module PriceCalculator
-  def self.checkout(_attribute)
-    0
+
+  UNIT_PRICE = 8
+
+  def self.checkout(cart)
+   cart.size * UNIT_PRICE * discount(cart)
+  end
+
+  def self.discount(cart)
+    return 1 - 0.05 if cart == cart.uniq && cart.size == 2
+    1
   end
 end
--- spec/price_calculator_spec.rb
+++ spec/price_calculator_spec.rb
@@ -1,7 +1,21 @@
 require_relative '../lib/price_calculator'
 
 describe PriceCalculator do
+  subject { PriceCalculator }
+
   it 'returns 0 when cart is empty' do
-    expect(PriceCalculator.checkout([])).to eq 0
+    expect(subject.checkout([])).to eq 0
+  end
+
+  it 'returns 8 when cart contains 1 book' do
+    expect(subject.checkout([1])).to eq 8
+  end
+
+  it 'returns 16 when cart contains 2 books' do
+    expect(subject.checkout([1,1])).to eq 16
+  end
+
+  it 'applies 5% discount when cart contains 2 distinct books' do
+    expect(subject.checkout([1,2])).to eq 15.2
   end
 end
